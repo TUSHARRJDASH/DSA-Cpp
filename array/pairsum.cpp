3a16a5095e73f79ec1f412937d28080a97a210be
ragnar11 <143804586+TUSHARRJDASH@users.noreply.github.com>
2026-06-28 20:17:05 +0530

    Implement twoSum function in pairsum.cpp

---
+++ array/pairsum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        int n=nums.size();
+        int i=0,j=n-1;
+        while(i<j){
+            int twoSum =nums[i]+nums[j];
+            if(twoSum>target){
+                j--;
+            }
+            else if(twoSum<target){
+                i++;
+            }
+            else{
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+                
+            }
+        }return ans;
+    }
+};
